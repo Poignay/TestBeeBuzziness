e6d231e73391ba8f1980f6eb27c2fda8d73513a6
Poignay <thomasmontoya@sfr.fr>
2018-08-11 19:17:21 +0200

    Version 1.0

---
+++ BeeBuzzinessTest/BeeBuzzinessTest/Nombre.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBuzzinessTest
+{
+    class Nombre
+    {
+        
+        private string ligne1="";
+        private string ligne2 ="";
+        private string ligne3 ="";
+
+        public Nombre()
+        {
+        }
+
+        public void GetNombre(char chiffre)
+        {
+            switch (chiffre)
+            {
+                case '1':
+                    ligne1 = ligne1 + "    ";
+                    ligne2 = ligne2 + "   |";
+                    ligne3 = ligne3 + "   |";
+                    break;
+                case '2':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + "  _|";
+                    ligne3 = ligne3 + " |_ ";
+                    break;
+                case '3':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + "  _|";
+                    ligne3 = ligne3 + "  _|";
+                    break;
+                case '4':
+                    ligne1 = ligne1 + "    ";
+                    ligne2 = ligne2 + " |_|";
+                    ligne3 = ligne3 + "   |";
+                    break;
+                case '5':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + " |_ ";
+                    ligne3 = ligne3 + "  _|";
+                    break;
+                case '6':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + " |_ ";
+                    ligne3 = ligne3 + " |_|";
+                    break;
+                case '7':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + "   |";
+                    ligne3 = ligne3 + "   |";
+                    break;
+                case '8':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + " [_|";
+                    ligne3 = ligne3 + " |_|";
+                    break;
+                case '9':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + " |_|";
+                    ligne3 = ligne3 + "   |";
+                    break;
+                case '0':
+                    ligne1 = ligne1 + "  _ ";
+                    ligne2 = ligne2 + " | |";
+                    ligne3 = ligne3 + " |_|";
+                    break;
+            }
+        }
+
+        public string GetLigne1()
+        {
+            return ligne1;
+        }
+        public string GetLigne2()
+        {
+            return ligne2;
+        }
+        public string GetLigne3()
+        {
+            return ligne3;
+        }
+
+    }
+}
---
+++ BeeBuzzinessTest/BeeBuzzinessTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBuzzinessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok = true;
+            while (ok)
+            {
+                Console.WriteLine("Veuillez saisir un nombre et valider avec la touche \"Entrée\" ou juste appuyer sur \"Entrée\" pour sortir");
+                string saisie = Console.ReadLine();
+                if (saisie != "")
+                {
+                    if (VerifSaisie(saisie) == true)
+                    {
+                        Nombre unNombre = Traitement(saisie);
+                        Console.WriteLine(unNombre.GetLigne1());
+                        Console.WriteLine(unNombre.GetLigne2());
+                        Console.WriteLine(unNombre.GetLigne3());
+                    }
+                }
+                else { ok = false; }
+            }
+        }
+
+        private static Nombre Traitement(string nombre)
+        {
+            Nombre unNombre = new Nombre();
+            for(int i = 0; i < nombre.Length; i++)
+            {
+                unNombre.GetNombre(nombre[i]);
+            }
+            return unNombre;
+        }
+
+        private static bool VerifSaisie(string saisie)
+        {
+            bool ok= true;
+            for (int i = 0; i < saisie.Length; i++)
+            { 
+                try
+                {
+                    int nombre =  Int32.Parse(saisie[i].ToString());
+                }
+                catch (Exception ex)
+                {
+                    ok = false;
+                }
+            }
+            return ok;
+        }
+    }
+}
